7b457c99b37476029f86c4a1462820f4847c9915
Owen <owenventer99@gmail.com>
2024-01-02 14:34:00 +0200

    html updates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 
 app = Flask(__name__)
 
@@ -15,5 +15,9 @@ def webhook():
 def get_latest():
     return jsonify(latest_data)
 
+@app.route('/')
+def index():
+    return send_from_directory('static', 'index.html')
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True)
