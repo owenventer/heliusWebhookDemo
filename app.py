cbab79299f46f00e2d5dced7e9b22df3231d016c
Owen <owenventer99@gmail.com>
2024-01-02 14:37:36 +0200

    html updates

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, send_from_directory
-
+import os
 app = Flask(__name__)
 
 # This will store the latest webhook data
@@ -19,5 +19,6 @@ def get_latest():
 def index():
     return send_from_directory('static', 'index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
